5339e944a5d3950fba489874b7e01d71c70c5ae0
Than Thanpanit <thanpa28@csail.mit.edu>
2025-12-27 00:46:38 -0500

    add analyzed.bin directory input

--- runner/scripts/summarize_analysis.py
+++ runner/scripts/summarize_analysis.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+import argparse
 import csv
 import dotenv
 import os
-from visualize import read_binary_output
+from rich import print
+from rich.progress import track
+from visualize import read_binary_output, generate_pdf_report
 
 dotenv.load_dotenv()
 
@@ -10,7 +13,17 @@ PROJECT_ROOT = os.getenv("PROJECT_ROOT")
 RUNNER_PATH = f"{PROJECT_ROOT}/runner"
 
 if __name__ == "__main__":
-    with open(f"{RUNNER_PATH}/analysis_summary.csv", "w", newline="") as csvfile:
+    parser = argparse.ArgumentParser(
+        description="Summarize analysis results into a CSV file."
+    )
+    parser.add_argument(
+        "analysis_dir", type=str, help="Directory containing analysis files"
+    )
+    args = parser.parse_args()
+    os.chdir(args.analysis_dir)
+    if not os.path.exists("summary"):
+        os.mkdir("summary")
+    with open("summary/analysis_summary.csv", "w", newline="") as csvfile:
         fieldnames = [
             "analysis_file",
             "total_txns",
@@ -41,12 +54,18 @@ if __name__ == "__main__":
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         writer.writeheader()
-        for analysis_file in sorted(os.listdir(f"{RUNNER_PATH}/analysis")):
+        for analysis_file in track(
+            sorted(os.listdir(".")), description="Processing analysis files..."
+        ):
             if not analysis_file.endswith(".bin"):
                 continue
             print(f"Summarizing {analysis_file}...")
 
-            writer.writerow(
-                read_binary_output(f"{RUNNER_PATH}/analysis/{analysis_file}")
-                | {"analysis_file": analysis_file}
+            analysis_data = read_binary_output(analysis_file)
+            writer.writerow(analysis_data | {"analysis_file": analysis_file})
+
+            print(f"Generating PDF report for {analysis_file}...")
+            generate_pdf_report(
+                analysis_data, f"summary/{analysis_file[:-4]}_report.pdf"
             )
+        print("Summary written to summary/analysis_summary.csv")
