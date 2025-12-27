2c84ccf86a7a8b1ea5891322f4628e41c627959e
Than Thanpanit <thanpa28@csail.mit.edu>
2025-12-27 00:25:20 -0500

    update test scripts for hardware simulation

--- runner/scripts/run_all_workloads.py
+++ runner/scripts/run_all_workloads.py
@@ -5,10 +5,12 @@ import asyncio
 import dotenv
 import itertools
 import os
-from tqdm import tqdm
+import subprocess
+from datetime import datetime
+from rich import print
+from rich.progress import track
 from dataclasses import dataclass
-import dataclasses
-from typing import Optional
+from typing import Optional, IO
 
 
 dotenv.load_dotenv()
@@ -17,6 +19,8 @@ PROJECT_ROOT = os.getenv("PROJECT_ROOT")
 RUNNER_PATH = f"{PROJECT_ROOT}/runner"
 DEFAULT_NUM_PUPPETS = 8
 DEFAULT_WORK_US = 0
+
+START_TIME = datetime.now().isoformat(timespec="seconds")
 
 
 @dataclass
@@ -79,21 +83,34 @@ class Options:
 BASE_OPTIONS = Options(stderr_status=True, work_us=5)
 
 # OPTIONS = [BASE_OPTIONS, dataclasses.replace(BASE_OPTIONS, sample_shift=8)]
-OPTIONS = [Options(work_us=5, sample_shift=8, worker_threads=16), Options(work_us=5, sample_shift=8, worker_threads=4)]
+SIM_OPTIONS = [
+    Options(work_us=5, sample_shift=8, worker_threads=16),
+    Options(work_us=5, sample_shift=8, worker_threads=4),
+]
+
+HW_OPTIONS = [Options(timeout=30)]
 
 
-async def stream_output(stream) -> None:
+async def stream_output(stream, file: Optional[IO[str]] = None) -> None:
     """Read async stream line by line and forward via tqdm.write()."""
     while True:
         line = await stream.readline()
         if not line:
             break
-        tqdm.write(line.decode().strip())
+        print(line.decode().strip(), file=file)
 
 
-async def run_workload(workload_file: str, log_file: str, options: Options) -> None:
-    analyzed_file = f"{RUNNER_PATH}/analysis/analyzed_{log_file}"
-    log_file = f"{RUNNER_PATH}/logs/{log_file}"
+async def execute_command(cmd, stream_stdout_to: Optional[IO[str]] = None) -> None:
+    proc = await asyncio.create_subprocess_exec(
+        *cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    await asyncio.gather(
+        stream_output(proc.stdout, file=stream_stdout_to), stream_output(proc.stderr)
+    )
+    await proc.wait()
+
+
+async def run_sim(workload_file: str, log_file: str, options: Options) -> None:
     cmd = [
         f"{RUNNER_PATH}/run",
         "--input",
@@ -102,8 +119,41 @@ async def run_workload(workload_file: str, log_file: str, options: Options) -> N
         log_file,
     ] + options.to_args()
     await execute_command(cmd)
+
+
+async def run_hw(
+    workload_file: str, log_file: str, options: Options, throttle: bool = False
+) -> None:
     cmd = [
-        f"{RUNNER_PATH}/bin/analyze",
+        f"{RUNNER_PATH}/hw_test",
+        workload_file,
+        "0",
+        "100" if throttle else "0",
+        str(options.timeout) if options.timeout is not None else "60",
+    ]
+    with open("hw_log.txt", "w") as hw_log:
+        await execute_command(cmd, stream_stdout_to=hw_log)
+
+    cmd = [
+        f"{RUNNER_PATH}/hwlog2bin",
+        "hw_log.txt",
+        log_file,
+        "125",  # fpga frequency in MHz
+    ]
+    await execute_command(cmd)
+
+
+async def run_workload(
+    workload_file: str, log_file: str, options: Options, use_hw: bool = False, throttle: bool = False
+) -> None:
+    analyzed_file = f"{RUNNER_PATH}/analysis/{START_TIME}/analyzed_{log_file}"
+    log_file = f"{RUNNER_PATH}/logs/{START_TIME}/{log_file}"
+    if use_hw:
+        await run_hw(workload_file, log_file, options, throttle=throttle)
+    else:
+        await run_sim(workload_file, log_file, options)
+    cmd = [
+        f"{RUNNER_PATH}/analyze",
         workload_file,
         log_file,
         str(
@@ -117,18 +167,14 @@ async def run_workload(workload_file: str, log_file: str, options: Options) -> N
     os.rename("analyzed.bin", analyzed_file)
 
 
-async def execute_command(cmd):
-    proc = await asyncio.create_subprocess_exec(
-        *cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-    )
-    await asyncio.gather(stream_output(proc.stdout), stream_output(proc.stderr))
-    await proc.wait()
-
-
 async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "workloads_dir", type=str, help="Directory containing workload files"
+    )
+    parser.add_argument("--hw", action="store_true", default=False, help="Run workloads with hw_test")
+    parser.add_argument(
+        "--latency", action="store_true", default=False, help="Throttle to measure latency"
     )
     args = parser.parse_args()
 
@@ -136,16 +182,22 @@ async def main():
         file for file in os.listdir(args.workloads_dir) if file.endswith(".bin")
     ]
 
-    for workload_file, options in tqdm(
-        itertools.product(workload_files, OPTIONS),
-        total=len(workload_files) * len(OPTIONS),
-        desc="Running and analyzing workloads",
+    os.mkdir(f"{RUNNER_PATH}/analysis/{START_TIME}")
+    os.mkdir(f"{RUNNER_PATH}/logs/{START_TIME}")
+
+    options = HW_OPTIONS if args.hw else SIM_OPTIONS
+    for workload_file, options in track(
+        itertools.product(workload_files, SIM_OPTIONS),
+        total=len(workload_files) * len(SIM_OPTIONS),
+        description="Running and analyzing workloads",
     ):
         workload_path = os.path.join(args.workloads_dir, workload_file)
         await run_workload(
             workload_path,
-            f"log_{workload_file}{options.to_filename_suffix()}.bin",
+            f"log_{workload_file[:-4]}{options.to_filename_suffix()}.bin",
             options,
+            use_hw=args.hw,
+            throttle=args.latency,
         )
 
 
